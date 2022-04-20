2f922c6bbf094f26794a0102b04a3fec6c2c7cf4
Shromann Majumder <shromannmajumder@gmail.com>
2022-04-20 22:18:36 +1000

    fix bugs

--- src/MDtoLatex.js
+++ src/MDtoLatex.js
@@ -11,13 +11,16 @@ export default function MDToLate(md) {
   md = md.replace(/(#\\section{([\w ]+)})/g, "\\subsection{$2}");
   md = md.replace(/(#\\subsection{([\w ]+)})/g, "\\subsubsection{$2}");
   
-  md = md.replace(/(\*)([\w ]+)\*/, "\\textbf{$2}");
-  md = md.replace(/(\*\\textbf{([\w ]+)}\*)/, "\\textit{$2}");
+  md = md.replace(/(\*)(.*?)/g, "\\item $2");
+  md = md.replace(/(\\item) (.*?)\\item/g, "\\textbf{$2}");
+  md = md.replace(/(\\textbf{}) (.*?)\\textbf{}/g, "\\textit{$2}");
+  
+  
   
   md = md.replace(/\\beq/g, "\\begin{align*}");
   md = md.replace(/\\eeq/g, "\\end{align*}");
   
-  md = md.replace(/(\*)(.*?)/g, "\\item $2");
+  
   md = md.replace(/(\\item [\w ]+\n)+/g, "\\begin{itemize}\n$&\\end{itemize}\n");
 
   return md;
